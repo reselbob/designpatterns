685b93342d1875d48344f3798110470444f30cdc
Bob Reselman <reselbob@gmail.com>
2020-08-06 18:07:29 -0700

    adding execute tests

--- command/tests/implementation_test.ts
+++ command/tests/implementation_test.ts
@@ -1,3 +1,4 @@
+import { ICommandStatus } from './../src/interface/commandStatus';
 
 import { expect } from 'chai';
 import {describe, it, before} from 'mocha';
@@ -11,15 +12,36 @@ describe('Command Tests', function() {
         expect(serverstart).to.be.an('object');
     })
 
-    it('Can init pauseserver', function() {
+    it('Can execute startserver', function() {
         const port: number = 8080;
-        const serverstart = new ServerPause(port);
-        expect(serverstart).to.be.an('object');
+        const serverstart = new ServerStart(port);
+        const result: ICommandStatus = serverstart.execute();
+        expect(result.statusCode).to.equal(200);
     })
 
     it('Can init pauseserver', function() {
         const port: number = 8080;
-        const serverstart = new ServerStop(port);
-        expect(serverstart).to.be.an('object');
+        const serverpause = new ServerPause(port);
+        expect(serverpause).to.be.an('object');
+    })
+
+    it('Can execute pauseserver', function() {
+        const port: number = 8080;
+        const serverpause = new ServerPause(port);
+        const result: ICommandStatus = serverpause.execute();
+        expect(result.statusCode).to.equal(200);
+    })
+
+    it('Can init stopserver', function() {
+        const port: number = 8080;
+        const serverstop = new ServerStop(port);
+        expect(serverstop).to.be.an('object');
+    })
+
+    it('Can execute stopserver', function() {
+        const port: number = 8080;
+        const serverstop = new ServerStop(port);
+        const result: ICommandStatus = serverstop.execute();
+        expect(result.statusCode).to.equal(200);
     })
 })
